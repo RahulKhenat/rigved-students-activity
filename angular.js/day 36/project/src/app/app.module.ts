1b869f94c0a6cbae04e4ddb94f34921130f01b1c
RahulKhenat <rahulkhenat2410@gmail.com>
2022-04-12 23:30:30 +0530

    day 36

---
+++ angular.js/day 36/project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ActivityComponent } from './activity/activity.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActivityComponent
+
+    
+  ],
+  imports: [
+    BrowserModule, 
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
